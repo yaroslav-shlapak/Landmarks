cbf794fc4f06b0192945ca5b08fb4672c8dd8f46
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-04-28 22:46:56 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yaroslav Shlapak on 28.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
