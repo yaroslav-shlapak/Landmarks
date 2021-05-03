5c3730c97c71d0957c739a3641acd0f970232143
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-05-03 16:49:35 +0300

    complete Part 2: lists and navigation

---
+++ Landmarks/View/LandmarksList.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarksList.swift
+//  Landmarks
+//
+//  Created by Yaroslav Shlapak on 03.05.2021.
+//
+
+import SwiftUI
+
+struct LandmarksList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+        }
+        .navigationTitle("Landmarks")
+    }
+}
+
+struct LandmarksList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarksList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+
+    }
+}
