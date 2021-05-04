f3cee4ed349879b3034213337e0008b2e8d20fd7
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-05-04 15:42:05 +0300

    Complete Part 4: Drawing paths and Shapes

---
+++ Landmarks/View/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Yaroslav Shlapak on 04.05.2021.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+     
+     var body: some View {
+         BadgeSymbol()
+             .padding(-60)
+             .rotationEffect(angle, anchor: .bottom)
+     }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
