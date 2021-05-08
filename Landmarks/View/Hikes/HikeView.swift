e19060340e8d862f35f4b7865aa826a8873506d3
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-05-08 23:38:54 +0300

    complete part 4: Animating Views and transitions

--- Landmarks/View/Hikes/HikeView.swift
+++ Landmarks/View/Hikes/HikeView.swift
@@ -7,9 +7,21 @@ A view displaying information about a hike, including an elevation graph.
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        let insertion = AnyTransition
+            .move(edge: .trailing)
+            .combined(with: .opacity)
+        let removal = AnyTransition
+            .scale
+            .combined(with: .opacity)
+        return .asymmetric(insertion: insertion, removal: removal)
+    }
+}
+
 struct HikeView: View {
     var hike: Hike
-    @State private var showDetail = false
+    @State private var showDetail = true
 
     var body: some View {
         VStack {
@@ -27,11 +39,14 @@ struct HikeView: View {
                 Spacer()
 
                 Button(action: {
-                    self.showDetail.toggle()
+                    withAnimation {
+                        self.showDetail.toggle()
+                    }
                 }) {
                     Image(systemName: "chevron.right.circle")
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
                 }
             }
@@ -47,8 +62,8 @@ struct HikeView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
             HikeView(hike: ModelData().hikes[0])
-                .padding()
-            Spacer()
+                .transition(.moveAndFade)
+
         }
     }
 }
